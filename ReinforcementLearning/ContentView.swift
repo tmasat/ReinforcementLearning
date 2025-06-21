6bfc30389d556573a9966ff8a5ed81b7be61cefd
Çağrı Tuğberk Masat <tugberkmasat@yahoo.com>
2025-06-21 19:02:07 +0300

    init project

---
+++ ReinforcementLearning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReinforcementLearning
+//
+//  Created by Tugberk Masat on 21.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReinforcementLearning/ReinforcementLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReinforcementLearningApp.swift
+//  ReinforcementLearning
+//
+//  Created by Tugberk Masat on 21.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct ReinforcementLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
